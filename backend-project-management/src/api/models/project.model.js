8479138d09329dad6c0bcd878816f68c6c01a670
emirandag <edulucho88@gmail.com>
2023-07-25 23:19:04 +0200

    Add owner attribute in project.model

--- backend-project-management/src/api/models/project.model.js
+++ backend-project-management/src/api/models/project.model.js
@@ -4,10 +4,11 @@ const { Schema } = mongoose
 const ProjectSchema = new Schema(
     {
         title: { type: String, required: true },
-        description: { type: String, required: true },
+        description: { type: String, required: true }, 
         users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
         tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
         createdDate: { type: Date, default: Date.now},
+        owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         isClosed: { type: Boolean, default: false, required: true }
     },
     {
